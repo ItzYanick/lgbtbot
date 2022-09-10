101d78edf514e5d12d480806ecc2c368c4198b08
ItzYanick <me@yanick.gay>
2022-09-10 17:23:39 +0200

    remove debugging

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -15,9 +15,6 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
-console.log("DEBUGING");
-console.log("token: " + process.env.DISCORD_TOKEN);
-
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 rest
